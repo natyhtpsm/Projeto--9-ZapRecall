bd65413a965386bdbe649e8ca6147c2d60361bad
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-05-16 09:35:00 -0300

    Initial Settings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import GlobalStyle from './components/GlobalStyle.jsx';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle>
+        
+      </GlobalStyle>
+    </>
+  )
+}
+
+export default App
