30a7aa9b10de0ee2828734297d60b38ae5103f87
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-05-19 16:22:02 -0300

    Update: data-test flashcard flashcard.jsx div cobrindo opened, closed and answer

--- src/components/flashcard.jsx
+++ src/components/flashcard.jsx
@@ -11,8 +11,9 @@ export default function Flashcard(props){
 
     const [cardColor, setCardColor] = useState('preto');
     const [cardState, setCardState] = useState('closed');
+    
 
-    if(cardState === 'closed'){return <Closed data-test="flashcard" number={index+1} setCardState={setCardState} cardColor={cardColor} />}
-    else if(cardState==='opened'){return <Opened data-test="flashcard" pergunta={question} setCardState={setCardState}/>}
-    else if(cardState==='answer'){return<Answer data-test="flashcard" resposta={answer} setCardState={setCardState} setCardColor={setCardColor} setContado={props.setContado} contado={props.contado}/>}
+    if(cardState === 'closed'){return <div data-test="flashcard"><Closed number={index+1} setCardState={setCardState} cardColor={cardColor} /></div>}
+    else if(cardState==='opened'){return <div data-test="flashcard"><Opened pergunta={question} setCardState={setCardState}/></div>}
+    else if(cardState==='answer'){return <div data-test="flashcard"><Answer resposta={answer} setCardState={setCardState} setCardColor={setCardColor} setContado={props.setContado} contado={props.contado}/></div>}
 }
